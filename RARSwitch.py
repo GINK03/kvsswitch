27c18d15f938da6908df2487ec63a32223915ec2
gimpei <gim.kobayashi@gmail.com>
2017-12-16 04:05:19 +0900

    a

--- RARSwitch.py
+++ RARSwitch.py
@@ -8,7 +8,7 @@ class RocksDB(object):
   def put(self, key:str, value:str):
     self.db.put(bytes(key,'utf8'), bytes(value, 'utf8')) 
   def get(self, key:str):
-    return self.db.get(bytes(key,'utf8')).decode('utf8')
+    return self.db.get(bytes(key,'utf8')).decode()
     
 import aerospike
 class Aerospike(object):
@@ -24,10 +24,21 @@ class Aerospike(object):
     key = ('hdd', self.name, key)
     (key, metadata, record) = self.client.get(key)
     return record.get('data')
-    
+
+import redis
+class Redis(object):
+  def __init__(self, name):
+    r = redis.StrictRedis(host='localhost', port=6379, db=0) 
+    self.r = r
+  def put(self, key:str, value:str):
+    self.r.set(key, value)
+  def get(self, key:str):
+    return self.r.get(key).decode()
 def as_open(type_name:str, file_name:str):
   if type_name == 'rocksdb':
     return RocksDB(file_name)
   elif type_name == 'aerospike':
     return Aerospike(file_name)
+  elif type_name == 'redis':
+    return Redis(file_name)
     
--- test.py
+++ test.py
@@ -13,3 +13,10 @@ aerospike-test
 db = RARSwitch.as_open('aerospike', 'test.db')
 db.put('3', '4')
 print(db.get('3'))
+
+'''
+redis-test
+'''
+db = RARSwitch.as_open('redis', 'test')
+db.put('5', '6')
+print(db.get('5'))
