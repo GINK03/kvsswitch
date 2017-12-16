8dfb60b80a08b94ad4f05eb6e1e105cb6342fdec
gimpei <gim.kobayashi@gmail.com>
2017-12-16 18:34:46 +0900

    a

--- setup.py
+++ setup.py
@@ -162,7 +162,7 @@ setup(
     # executes the function `main` from this package when invoked:
     entry_points={  # Optional
         'console_scripts': [
-            'sample=sample:main',
+            'src=src:main',
         ],
     },
 )
